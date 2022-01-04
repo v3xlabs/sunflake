f9a9a114650949d75c8e56ac2b80aeade61a993c
Prokop Schield <76836484+prokopschield@users.noreply.github.com>
2022-01-04 14:55:30 +0100

    Introduce general formatting (#12)
    Co-authored-by: Antony <antoniostignjedec@gmail.com>

--- tests/sunflake.test.ts
+++ tests/sunflake.test.ts
@@ -12,50 +12,47 @@ describe('Promise', () => {
     it('Generates two snowflake value', () => {
         const flake1 = snowflake();
         const flake2 = snowflake();
-    
+
         expect(flake1 != flake2);
     });
-    
+
     it('Generates two snowflake value in sync', async () => {
-        const [flake1, flake2] = await Promise.all([
-            snowflake(),
-            snowflake()
-        ]);
-    
+        const [flake1, flake2] = await Promise.all([snowflake(), snowflake()]);
+
         expect(flake1 != flake2);
     });
-    
+
     it('Generates two snowflake value in sync with same time', async () => {
         const time = Date.now();
         const [flake1, flake2] = await Promise.all([
             snowflake(time),
             snowflake(time),
         ]);
-    
+
         expect(flake1 !== flake2);
     });
-    
+
     it('Generates 500 snowflake value in sync with same time', async () => {
         const time = Date.now();
         const hugeList = [];
         for (let i = 0; i <= 500; i++) {
             hugeList.push(snowflake(time));
         }
-        
+
         const list = await Promise.all(hugeList);
-    
+
         expect(new Set(list).size === list.length);
     });
-    
+
     it('Generates 5200 snowflake value in sync with same time', async () => {
         const time = Date.now();
         const hugeList = [];
         for (let i = 0; i <= 5200; i++) {
             hugeList.push(snowflake(time));
         }
-    
+
         const list = await Promise.all(hugeList);
-    
+
         expect(new Set(list).size === list.length);
     });
 });
