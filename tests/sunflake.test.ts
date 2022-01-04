c18f4e6b75e0d591cbc6d09ddda280dbab168d26
Prokop <prokop.schield@protonmail.com>
2022-01-04 19:32:12 +0100

    Introduce overflow test

--- tests/sunflake.test.ts
+++ tests/sunflake.test.ts
@@ -13,13 +13,13 @@ describe('Promise', () => {
         const flake1 = snowflake();
         const flake2 = snowflake();
 
-        expect(flake1 != flake2);
+        expect(flake1 != flake2).toBe(true);
     });
 
     it('Generates two snowflake value in sync', async () => {
         const [flake1, flake2] = await Promise.all([snowflake(), snowflake()]);
 
-        expect(flake1 != flake2);
+        expect(flake1 != flake2).toBe(true);
     });
 
     it('Generates two snowflake value in sync with same time', async () => {
@@ -29,7 +29,7 @@ describe('Promise', () => {
             snowflake(time),
         ]);
 
-        expect(flake1 !== flake2);
+        expect(flake1 !== flake2).toBe(true);
     });
 
     it('Generates 500 snowflake value in sync with same time', async () => {
@@ -41,18 +41,29 @@ describe('Promise', () => {
 
         const list = await Promise.all(hugeList);
 
-        expect(new Set(list).size === list.length);
+        expect(new Set(list).size).toBe(list.length);
     });
 
     it('Generates 5200 snowflake value in sync with same time', async () => {
         const time = Date.now();
         const hugeList = [];
-        for (let i = 0; i <= 5200; i++) {
+        for (let i = 0; i < 5200; i++) {
             hugeList.push(snowflake(time));
         }
 
         const list = await Promise.all(hugeList);
 
-        expect(new Set(list).size === list.length);
+        expect(new Set(list).size).toBe(list.length);
+    });
+
+    it('Test machineID overflow', () => {
+        const some_constant = 83196983689;
+        const snowflake = generateSunflake({
+            machineID: 1025,
+            epoch: 0,
+        });
+        expect(snowflake(some_constant)).toBe(
+            String((BigInt(some_constant) << 22n) + (1n << 12n))
+        );
     });
 });
