5a4f7656e536fadff3f6de86d5fe7f6764f275ac
Prokop Schield <76836484+prokopschield@users.noreply.github.com>
2022-01-05 14:09:13 +0100

    Introduce decode() and update tests (#21)
    * Introduce decode()
    
    * Introduce tests for decode()
    
    * Adjust eslint
    
    Co-authored-by: Antony1060 <antoniostignjedec@gmail.com>
    
    Co-authored-by: Lucemans <luc@lucemans.nl>
    Co-authored-by: Antony1060 <antoniostignjedec@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,30 @@
-export type SunflakeConfig = {
-    /**
-     * Machine ID
-     * Must be between 0 and 1023.
-     * Used to identify the ID that the snowflake was generated on and prevent collision.
-     * @default 1
-     */
-    machineID?: bigint | number;
+export type EpochConfig = {
     /**
      * Epoch
      * is a custom time value for when ID generation should commence.
      * Generally speaking this is set to some date (in the past).
      * @default 1640995200000
      */
-    epoch?: bigint | number;
+    epoch?: bigint | number | string;
 };
 
+export type SunflakeConfig = EpochConfig & {
+    /**
+     * Machine ID
+     * Must be between 0 and 1023.
+     * Used to identify the ID that the snowflake was generated on and prevent collision.
+     * @default 1
+     */
+    machineId?: bigint | number | string;
+};
+
+export const DEFAULT_EPOCH = 1_640_995_200_000n;
+
 export const generateSunflake = (
-    config: SunflakeConfig
+    config?: SunflakeConfig
 ): ((time?: bigint | number) => string) => {
-    const machineID = BigInt(config?.machineID ?? 1) & 1023n;
-    const epoch = BigInt(config?.epoch ?? 1_640_995_200_000);
+    const machineId = BigInt(config?.machineId ?? 1) & 1023n;
+    const epoch = BigInt(config?.epoch ?? DEFAULT_EPOCH);
 
     let lastTime = 0n;
     let seq = 0n;
@@ -43,6 +48,23 @@ export const generateSunflake = (
         }
 
         // generate sunflake
-        return String((currentTime << 22n) | (machineID << 12n) | seq);
+        return String((currentTime << 22n) | (machineId << 12n) | seq);
     };
+};
+
+export const decode = (
+    sunflake: bigint | number | string,
+    config?: EpochConfig
+) => {
+    const epoch = BigInt(config?.epoch || DEFAULT_EPOCH);
+    let snowflake = BigInt(sunflake);
+    const seq = snowflake & 4095n;
+
+    snowflake >>= 12n;
+    const machineId = snowflake & 1023n;
+
+    snowflake >>= 10n;
+    const time = epoch + snowflake;
+
+    return { time, machineId, seq, epoch };
 };
